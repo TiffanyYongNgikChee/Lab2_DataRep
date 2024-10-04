706105f93df99dbcd7e23d535e332d7b4d038338
TiffanyYongNgikChee <G00425067@atu.ie>
2024-10-04 08:55:26 +0000

    exercise 3 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
-  let x =5;
-
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
