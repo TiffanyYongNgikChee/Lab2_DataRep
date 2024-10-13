5c8871baf169082c5b12a9f46eedc59dced13262
TiffanyYongNgikChee <G00425067@atu.ie>
2024-10-13 14:23:08 +0000

    add some comments

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+// Import the Card component from the react-bootstrap library for UI styling
 import Card from 'react-bootstrap/Card';
 
-const MovieItem = (props)=>{
-
-   
-    return(
+// Define the MovieItem component, which receives props as an argument
+const MovieItem = (props) => {
+    // Render a Card component to display movie details
+    // The Card component is styled using inline styles with a fixed width
+    return (
         <Card style={{ width: '18rem' }}>
+            {/* Display the movie's poster image using the Card.Img component */}
+            {/* Access the movie poster from props.myMovies.Poster */}
             <Card.Img variant="top" src={props.myMovies.Poster} />
+            
             <Card.Body>
+                {/* Display the movie title using the Card.Title component */}
                 <Card.Title>{props.myMovies.Title}</Card.Title>
+                
+                {/* Display the movie's release year using the Card.Text component */}
                 <Card.Text>
-                {props.myMovies.Year}
+                    {props.myMovies.Year}
                 </Card.Text>
             </Card.Body>
         </Card>
     );
 }
+
+// Export the MovieItem component to be used in other parts of the application
 export default MovieItem;
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,10 +1,18 @@
+// Import the MovieItem component from the movieitem file
 import MovieItem from "./movieitem";
 
-const Movies = (props)=>{
+// Define the Movies component, which receives props as an argument
+const Movies = (props) => {
+    // Map over the myMovies array from props to render a list of MovieItem components
     return props.myMovies.map(
-        (movie)=>{
+        (movie) => {
+            // Render a MovieItem component for each movie in the array
+            // Pass the movie object as a prop (myMovies)
+            // Use the unique imdbID as the key to avoid the "missing key" warning
             return <MovieItem myMovies={movie} key={movie.imdbID}/>
         }
-    )
+    );
 }
+
+// Export the Movies component for use in other parts of the application
 export default Movies;
