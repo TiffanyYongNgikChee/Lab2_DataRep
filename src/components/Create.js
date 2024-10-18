0b553e3fc94d1e325418b3dbea2d26cd0860b72a
TiffanyYongNgikChee <G00425067@atu.ie>
2024-10-18 09:14:46 +0000

    Lab4: handle the new events from button and log info to console

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
-    return <h3>Hello from Create component</h3>;
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(title);
+  }
+
+    return (
+      <div>
+        <h3>Hello from create component</h3>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Add Movie Title: </label>
+            <input type="text"
+              className="form-control"
+              value={title}
+              onChange={(e) => {setTitle(e.target.value)}} 
+            />
+          </div>
+
+          <div>
+            <input type="submit" value="Add Movie"></input>
+          </div>
+
+        </form>
+      </div>
+    );
   };
   
   export default Create;
