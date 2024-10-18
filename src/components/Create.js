f53578b99ce28af3efdc04fda55f29f82bd7faa8
TiffanyYongNgikChee <G00425067@atu.ie>
2024-10-18 09:25:15 +0000

    Lab4: add fields for movie year and movie poster

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const Create = () => {
   const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
 
   const handleSubmit = (e) =>{
     e.preventDefault();
     console.log(title);
+    console.log(year);
+    console.log(poster);
   }
 
     return (
@@ -13,12 +17,31 @@ const Create = () => {
         <h3>Hello from create component</h3>
 
         <form onSubmit={handleSubmit}>
+          {/*Movie Title */}
           <div className="form-group">
             <label>Add Movie Title: </label>
             <input type="text"
               className="form-control"
               value={title}
               onChange={(e) => {setTitle(e.target.value)}} 
+            />
+          </div>
+          {/*Movie Year */}
+          <div className="form-group">
+            <label>Add Movie Year: </label>
+            <input type="text"
+              className="form-control"
+              value={year}
+              onChange={(e) => {setYear(e.target.value)}} 
+            />
+          </div>
+          {/*Movie Poster */}
+          <div className="form-group">
+            <label>Add Movie Poster: </label>
+            <input type="text"
+              className="form-control"
+              value={poster}
+              onChange={(e) => {setPoster(e.target.value)}} 
             />
           </div>
 
